e912574c877b07d23b67aa860db593d4acb315c4
Sathya Pramod K S <67020305+sathyapramodks@users.noreply.github.com>
2021-08-07 17:41:44 +0530

    Add files via upload

--- gmeetLinkcomp/settings.py
+++ gmeetLinkcomp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3c%aohbl7y*%(=^o#(xzww3d@2d0@8b-@j3sqvt&a)*=l(4k%*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
